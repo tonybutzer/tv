fee58d2dad8fcdddd815d5bfe06edfaf2cab509c
tony <tonybutzer@gmail.com>
2022-12-12 11:59:55 -0600

    sleepy work 2022 November

--- docker_install_ubuntu.sh
+++ docker_install_ubuntu.sh
@@ -1,22 +1,6 @@
 #! /bin/bash
-
-# sudo apt update
-# sudo apt upgrade
-
-#PREREQUISITES
-# sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# for ubuntu docker
 
 
- 293  sudo apt update
-  294  apt-cache policy docker-ce
-  295  sudo apt install docker-ce
-  296  sudo systemctl status docker
-  297  docker-version
-  298  docker --version
-  299  docker ps
-  300  sudo docker run hello-world
-
+sudo apt-get install docker*
+sudo usermod -aG docker $USER
